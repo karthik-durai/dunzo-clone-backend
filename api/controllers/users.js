9b9614ff1ff979d7ec8114bd396caf767e3db828
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-23 01:19:41 +0530

    implement function and variables to compose url, get access token and user info

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,5 +1,44 @@
-function getUserProfile (req, res) {
-  console.log(req.body)
+const {clientCredentials} = require('../../googleOauthCredentials')
+const {google} = require('googleapis')
+
+const redirectURI = 'http://localhost:8000/user/oauthcallback'
+
+const scopes = ['https://www.googleapis.com/auth/plus.me',
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/userinfo.profile'
+]
+
+const oauth2Client = new google.auth.OAuth2(
+  clientCredentials.clientID,
+  clientCredentials.clientSecret,
+  redirectURI
+)
+
+const oauth2 = google.oauth2({
+  auth: oauth2Client,
+  version: 'v2'
+})
+
+const url = oauth2Client.generateAuthUrl({
+  access_type: 'offline',
+  scope: scopes
+})
+
+function sendSigninLink (req, res) {
+  console.log(url)
 }
 
-module.exports = { getUserProfile }
+function getToken (req, res) {
+  oauth2Client.getToken(req.query.code).then(obj => {
+    oauth2Client.setCredentials(obj.tokens)
+    oauth2.userinfo.v2.me.get((err, res) => {
+      if (err) {
+        console.error(err)
+      } else {
+        console.log(res)
+      }
+    })
+  })
+}
+
+module.exports = { getToken, sendSigninLink }
