c24a7224ca23c82bf41e0477479302e56656d912
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-23 01:20:09 +0530

    add /oauthcallback route

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const usersController = require('../controllers/users')
 
-router.post('/authtoken', usersController.getUserProfile)
+router.get('/', usersController.sendSigninLink)
+router.get('/oauthcallback', usersController.getToken)
 
 module.exports = router
