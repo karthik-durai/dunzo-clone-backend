883bfef0c12086aeac4896fe2aef77ff258da293
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-24 06:30:25 +0530

    insert DB connection code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const path = require('path')
+const mongoose = require('mongoose')
 
 const usersRoute = require('./api/routes/users')
 
@@ -10,5 +11,12 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.use('/', express.static(path.join(__dirname, 'view', 'public')))
+app.use('/user', usersRoute)
+
+mongoose.connect('mongodb://localhost:27017/dunzoClone', { useNewUrlParser: true })
+
+let db = mongoose.connection
+
+db.on('open', () => { console.log('connected to dunzo clone db') })
 
 app.listen(8000, () => console.log('listening on port 8000'))
