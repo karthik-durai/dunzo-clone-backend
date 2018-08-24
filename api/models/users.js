3e1e502256fe5904a7a4477d70fa8862067bb013
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-24 12:37:10 +0530

    include profile picture field

--- api/models/users.js
+++ api/models/users.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   emailID: { type: String, required: true },
+  profilePicture: { type: String },
   currentOrder: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' },
   pastOrders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
   jwt: { type: String, default: null }
