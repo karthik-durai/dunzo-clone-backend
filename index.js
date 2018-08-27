cae8f4b928cd5f350d46dbc1ac5d9b7e03616426
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-27 21:13:56 +0530

    insert middlewares to authenticate and handle req for home

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
+const { authenticate } = require('./api/authentication/authenticate')
+const { handleHomePageRequest } = require('./api/controllers/users')
 
 const usersRoute = require('./api/routes/users')
 
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-app.use('/', express.static(path.join(__dirname, 'view', 'public')))
+app.use('/', authenticate,
+  handleHomePageRequest,
+  express.static(path.join(__dirname, 'view', 'public')))
+
 app.use('/user', usersRoute)
 
 mongoose.connect('mongodb://localhost:27017/dunzoClone', { useNewUrlParser: true })
