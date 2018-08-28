8e1959b90af38148c95c6d75279d20e4d2bee5c4
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-28 14:04:48 +0530

    add /user/ mount point

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,6 +6,7 @@ router.get('/oauthcallback', usersController.getToken)
 router.post('/placeorder', authenticate, usersController.placeOrder)
 //  router.get('/showOrders', authenticate, usersController.showOrders)
 //  router.get('/trackOrder', authenticate)
+router.get('/', authenticate, usersController.serveOrdersPage)
 router.get('/signout', authenticate, usersController.signout)
 
 module.exports = router
