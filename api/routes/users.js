88138ea9a1e91cd32f0d44014ca2571686a940a6
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-28 19:44:00 +0530

    move '/' statement to top

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,9 +2,9 @@ const router = require('express').Router()
 const usersController = require('../controllers/users')
 const { authenticate } = require('../authentication/authenticate')
 
+router.get('/', authenticate, usersController.serveOrdersPage)
 router.get('/oauthcallback', usersController.handleUserInfo)
 router.post('/placeorder', authenticate, usersController.placeOrder)
-router.get('/', authenticate, usersController.serveOrdersPage)
 router.get('/signout', authenticate, usersController.signout)
 
 module.exports = router
