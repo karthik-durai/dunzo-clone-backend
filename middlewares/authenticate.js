a0d1fca67b391115156b0d97d81fd8b465b7415c
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-09-02 00:38:01 +0530

    move middleware dir to outside of api

---
+++ middlewares/authenticate.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/users')
+const { privateKey } = require('../../secrets/jwtPrivateKey')
+
+async function authenticate (req, res, next) {
+  try {
+    var decodedJWT = jwt.verify(req.cookies.access_token, privateKey)
+    req.isSignedIn = await checkForJWT(decodedJWT, req.cookies.access_token)
+    req.emailID = decodedJWT.email
+  } catch (error) {
+    req.isSignedIn = false
+  }
+  next()
+}
+
+async function checkForJWT (userinfo, jwToken) {
+  try {
+    let searchResult = (await User.findOne({ emailID: userinfo.email }).exec())
+    if (searchResult && searchResult.jwt === jwToken) {
+      return true
+    }
+    return false
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = { authenticate }
